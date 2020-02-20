3674e11416f524b8dbf136d3723cb05cce81d283
Julien MIFSUD <julien.mifsud77@gmail.com>
2020-02-20 17:19:48 +0100

    new loader

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import './style.css'
 
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { shuffle } from 'lodash'
+
+const spring = {
+  type: 'spring',
+  damping: 20,
+  stiffness: 300
+}
+
 const Loader = () => {
+  const [colors, setColors] = useState(initialColors)
+
+  useEffect(() => {
+    setTimeout(() => setColors(shuffle(colors)), 300)
+  }, [colors])
   return (
-    <div className='loader center center-screen'>
-      <i
-        style={{ color: 'teal' }}
-        className='fa fa-circle-notch fa-spin fa-5x fa-fw'
-      />
+    <div className='center-screen'>
+      <ul className='ul-loader'>
+        {colors.map(background => (
+          <motion.li
+            key={background}
+            layoutTransition={spring}
+            style={{ background }}
+          />
+        ))}
+      </ul>
     </div>
   )
 }
-
+const initialColors = ['#b61c1c', '#9f0d0d', '#960202', '#7c0303']
 export default Loader
