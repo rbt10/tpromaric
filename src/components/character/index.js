6b516413d573f7d3addf98d5da167de8d4875a2c
Julien MIFSUD <julien.mifsud77@gmail.com>
2020-02-21 13:20:34 +0100

    love animation

--- src/components/character/index.js
+++ src/components/character/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './character.css'
+import { motion, useAnimation, use } from 'framer-motion'
 
-const Character = ({ character, history }) => {
+const Character = ({ character, favoris, history }) => {
+  const [fav, setFav] = useState(false)
+  const controls = useAnimation()
   const redirectToDetail = id => {
     history.push(`/characters/${id}`)
+  }
+
+  const add = () => {
+    setFav(!fav)
+    controls.set('visible')
+    controls.start({
+      rotate: 360,
+      transition: {
+        duration: 0.5
+      }
+    })
+    controls.mount()
+    console.log('ici')
+    // controls.stop()
   }
 
   return (
@@ -14,7 +31,14 @@ const Character = ({ character, history }) => {
         src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
       />
       <div className='detail-character'>
-        <strong>{character.name}</strong>
+        <div className='detail-character-header'>
+          <strong>{character.name}</strong>
+          <motion.i
+            onClick={add}
+            className={`fa-heart ${fav ? 'fas icon-color' : 'far'}`}
+            animate={controls}
+          />
+        </div>
         <div className='detail-character--description'>
           {character.description}
         </div>
