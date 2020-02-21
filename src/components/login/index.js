aa483423f4818a382ef3dfa65040aba888385359
canserkanuren <canserkan.uren@gmail.com>
2020-02-21 09:55:06 +0100

    :bug: routes protegees

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './login.css'
 import Logo from '../logo'
 import LoginForm from '../loginForm'
 
 export default function Login(props) {
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      props.history.push('characters')
+    }
+  }, [])
   return (
     <div className='login-backgound'>
       <Logo></Logo>
